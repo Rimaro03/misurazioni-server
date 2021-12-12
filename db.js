e35d70797868574680d6eee0deb4bdf0f6fbd5ca
Rimaro <leonardo.ongaro2003@hotmail.com>
2021-12-12 10:21:41 +0100

    removed username and apssword, server.py => server.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+const config = require('./config.json');
 
 let db = mysql.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database
 });
 
 db.connect((err) => {
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const db = require('./db')
 
-while (True) {
-    temperatura = Math.random() * (40 - 30) + 30
-    umidita = Math.random() * (100 - 0) + 0
-    timestamp = new Date()
+setInterval(() => {
+    temperatura = Math.round(Math.random() * (40 - 30) + 30)
+    umidita = Math.round(Math.random() * (100 - 0) + 0)
+    timestamp = new Date().getTime() 
+    console.log(temperatura, umidita, timestamp);
 
     db.query(`INSERT INTO misure (temperatura, umidita, timestamp) VALUES (${temperatura}, ${umidita}, ${timestamp})`, (err, result) => {
         if (err) throw err
     });
-
-    sleep(5000)
-}
+}, 2000);
